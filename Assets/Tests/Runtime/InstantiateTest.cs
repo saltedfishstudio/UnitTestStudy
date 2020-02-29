4d2f54b1f295782015a629de7c4d38ffe234b9d3
saltedfishstudio <saltedfishstudio@pm.me>
2020-02-29 16:23:38 +0900

    유닛 테스트 예제

---
+++ Assets/Tests/Runtime/InstantiateTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	public class InstantiateTest
+	{
+		[Test]
+		public void InstantiatedGameObject()
+		{
+			var go = new GameObject("MyGameObject");
+			Assert.AreEqual("MyGameObject", go.name);
+		}
+
+		[UnityTest]
+		public IEnumerator InstantiateGameObjectWithEnumerator()
+		{
+			var go = new GameObject("MyGameObject");
+			go.AddComponent<Rigidbody>();
+			var originalPosition = go.transform.position.y;
+            
+			yield return new WaitForFixedUpdate();
+            
+			Assert.AreNotEqual(originalPosition, go.transform.position.y);
+		}
+        
+	}
+}
---
+++ Assets/Tests/Runtime/SkillTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.VersionControl;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SkillTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SkillTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SkillTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
