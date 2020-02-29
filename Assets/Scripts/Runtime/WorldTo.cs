20dc8ac97ec85b7f345a17aa615af0a0a00775ad
saltedfishstudio <saltedfishstudio@pm.me>
2020-02-29 23:04:53 +0900

    유닛 테스트 예제

---
+++ Assets/Scripts/Runtime/WorldTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTo : MonoBehaviour
+{
+    Camera camera;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewport = camera.WorldToViewportPoint(transform.position);
+        Debug.Log($"{viewport.x}, {viewport.y}, {viewport.z}");
+    }
+}
